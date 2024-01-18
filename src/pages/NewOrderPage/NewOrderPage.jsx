87e7abe54ff2b2a202e4a5e6aa7dff83e7a66943
amayorga7904 <alexmayorga20@gmail.com>
2024-01-17 17:42:29 -0800

    added useEffect and button to new order page

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,5 +1,17 @@
+import { useState, useEffect } from "react";
+
 export default function NewOrderPage() {
+  const [storeItems, setStoreItems] = useState([])
+
+  // - Fetch the storeItems from the server via AJAX
+  // - When the data comes back, call setStoreItems to save in state
+  useEffect(function() {
+    console.log('new order page is rendered')
+  })
   return (
-    <h1>NewOrderPage</h1>
+    <>
+      <h1>NewOrderPage</h1>
+      <button onClick={setStoreItems}>Trigger re-render</button>
+    </>
   );
 }
